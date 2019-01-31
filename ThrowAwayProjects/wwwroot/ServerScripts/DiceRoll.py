97e99faf72e55b48bcb627e06b4f66e9d53cee33
Daniel Stoughton <Daniel.Stoughton@gmail.com>
2019-01-31 09:24:56 -0600

    formatted the script to be more clean

--- ThrowAwayProjects/wwwroot/ServerScripts/DiceRoll.py
+++ ThrowAwayProjects/wwwroot/ServerScripts/DiceRoll.py
@@ -2,15 +2,23 @@ import sys
 import random
 from datetime import datetime
 
+
 def roll_check(modifier, target):
+
     random.seed(datetime.now())
-    roll = random.randrange(1,6) + random.randrange(1,6) + random.randrange(1,6)
+    roll = 0
+
+    for i in range(1, 3):
+        roll = roll + random.randrange(1, 6)
+
     if roll == 3 or roll == 4 or roll == 5:
         return "Critical Failure"
     elif roll == 16 or roll == 17 or roll == 18:
         return "Critical Success"
     else:
         roll = roll + int(modifier)
+
     return "Success" if roll >= int(target) else "Failure"
 
-print(roll_check(sys.argv[1],sys.argv[2]))
+
+print(roll_check(sys.argv[1], sys.argv[2]))
