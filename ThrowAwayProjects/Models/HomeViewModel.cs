698e21d4ec25342869e0471f92eb5cee2b07818e
Daniel Stoughton <Daniel.Stoughton@gmail.com>
2019-04-08 15:13:21 -0500

    updated the style of the home page to make it look more like a forum

--- ThrowAwayProjects/Models/HomeViewModel.cs
+++ ThrowAwayProjects/Models/HomeViewModel.cs
@@ -3,13 +3,15 @@ namespace ThrowAwayProjects.Models
 {
     public class HomeViewModel : BaseViewModel
     {
+        public string AccountStatus { get; set; }
+        public string Privilege { get; set; }
         public DateTime DateSessionStarted { get; set; }
-        public int AccountAge { get; set; }
 
         public HomeViewModel()
         {
+            AccountStatus = "Not Logged In";
+            Privilege = "Not Logged In";
             DateSessionStarted = DateTime.Now;
-            AccountAge = 0;
         }
     }
 }
