fd487d9366070a3f6810ee7025311c79687b7a44
Daniel Stoughton <Daniel.Stoughton@gmail.com>
2019-05-15 16:52:42 -0500

    Added logic to save a thread to the db

--- ThrowAwayProjects/Controllers/ForumController.cs
+++ ThrowAwayProjects/Controllers/ForumController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Hosting;
@@ -7,6 +6,8 @@ using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Microsoft.Extensions.Configuration;
 using ThrowAwayProjects.Models;
 using Microsoft.AspNetCore.Http;
+using ThrowAwayData;
+using Newtonsoft.Json;
 
 namespace ThrowAwayProjects.Controllers
 {
@@ -16,19 +17,10 @@ namespace ThrowAwayProjects.Controllers
         base(viewEngine, configuration, environment)
         {
         }
-
-        public ActionResult Index()
-        {
-            return HandleExceptions(() =>
-            {
-                var model = new List<PostViewModel>();
-                return View();
-            });
-        }
-
+        
         public JsonResult AddEditThread(int Id)
         {
-            return HandleExceptions(() => 
+            return HandleExceptions(() =>
             {
                 HttpContext.Session.SetString("CurrentEditId", Id.ToString());
 
@@ -39,7 +31,7 @@ namespace ThrowAwayProjects.Controllers
 
                 if (DBthread == null)
                     throw new Exception("That thread couldn't be found in the database.");
-                
+
                 var viewModel = new ThreadViewModel()
                 {
                     Title = DBthread.Title,
@@ -47,6 +39,39 @@ namespace ThrowAwayProjects.Controllers
                 };
 
                 return Modal("_AddEditThread", viewModel);
+            });
+        }
+
+        [HttpPost]
+        public ActionResult AddEditThread(ThreadViewModel viewModel)
+        {
+            return HandleExceptions(() =>
+            {
+                var key = HttpContext.Session.GetString("UserKey");
+
+                if (key == null)
+                    throw new Exception("You must be logged in to do that.");
+
+                var user = JsonConvert.DeserializeObject<UserIdentity>(key);
+                var sessionString = HttpContext.Session.GetString("CurrentEditId");
+                var dbThread = new Thread();
+
+                if (sessionString == null)
+                {
+                    dbThread.Title = viewModel.Title;
+                    dbThread.Body = viewModel.Body;
+                    dbThread.CreatedBy = user.Id ?? 0;
+                    database.Threads.Add(dbThread);
+                }
+                else
+                {
+                    dbThread = database.Threads.Get(Convert.ToInt32(sessionString));
+                    dbThread.Title = viewModel.Title;
+                    dbThread.Body = viewModel.Body;
+                    database.Threads.Edit(dbThread);
+                }
+
+                return RedirectToAction("Thread");
             });
         }
     }
