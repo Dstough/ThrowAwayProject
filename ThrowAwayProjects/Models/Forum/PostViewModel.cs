eaf1f5f53e03bccb9cf88b3ad6b3940541397c30
Daniel Stoughton <Daniel.Stoughton@gmail.com>
2019-10-25 16:31:03 -0500

    started to re-work the thread view page to be more in-line with the jackpoint published books.

--- ThrowAwayProjects/Models/Forum/PostViewModel.cs
+++ ThrowAwayProjects/Models/Forum/PostViewModel.cs
@@ -5,6 +5,7 @@ namespace ThrowAwayProjects.Models
     public class PostViewModel
     {
         public int? Id { get; set; }
+        public string Section { get; set; }
         public string Body { get; set; }
         public string Author { get; set; }
         public string CSS { get; set; }
