126cfea4e59f87ba6a8a657c52019275426311c2
Daniel <Daniel.Stoughton@gmail.com>
2019-12-14 15:09:22 -0600

    Added the deamon to take the place of sql jobs and maybe more.

---
+++ ThrowAwayDaemon/Worker.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace ThrowAwayDaemon
+{
+    public class Worker : IHostedService, IDisposable
+    {
+        private Timer timer;
+        private int interval;
+
+        public Worker()
+        {
+            interval = 1000 * 5;
+            timer = new Timer(Main, "started", Timeout.Infinite, Timeout.Infinite);
+        }
+        public void Dispose() => timer.Dispose();
+
+        #region Service Framework
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            timer.Change(5000, interval);
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            timer.Change(Timeout.Infinite, Timeout.Infinite);
+            return Task.CompletedTask;
+        }
+
+        #endregion
+
+        public void Main(object state)
+        {
+            //TODO: All SQLite Jobs would go here.
+        }
+    }
+}
