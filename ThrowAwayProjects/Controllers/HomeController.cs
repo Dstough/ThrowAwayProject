83c33bc06f94c4cb24adf2b6595711cbfae0555f
Daniel Stoughton <Daniel.Stoughton@gmail.com>
2020-01-29 17:07:19 -0600

    Added the base layout for articles

--- ThrowAwayProjects/Controllers/HomeController.cs
+++ ThrowAwayProjects/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace ThrowAwayProjects.Controllers
             {
                 var viewModel = new HomeViewModel();
 
-                foreach (var thread in database.Threads.Find(3))
+                foreach (var thread in database.Threads.Find(5))
                 {
                     var author = database.Users.Get(thread.CreatedBy);
                     var group = database.UserGroups.Get(author.UserGroupId);
