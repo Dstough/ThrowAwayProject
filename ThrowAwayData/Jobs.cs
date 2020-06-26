1ff68592e5fd6b500f23cee645daa467152326f6
Daniel <Daniel.Stoughton@gmail.com>
2020-06-26 02:06:49 -0500

    Removed unused logging

--- ThrowAwayData/Jobs.cs
+++ ThrowAwayData/Jobs.cs
@@ -14,19 +14,14 @@ namespace ThrowAwayData
                     .Find()
                     .Where(item => item.CreatedOn <= DateTime.Now.AddDays(-7));
 
-                Console.WriteLine($"Found {dbThreads.Count()} threads to close");
-
                 foreach (var item in dbThreads)
                 {
                     item.Closed = true;
                     Threads.Edit(item);
-
-                    Console.WriteLine($"Closed Thread {item.PublicId}");
                 }
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                Console.WriteLine(ex.ToString().Replace(" at ", "\n"));
             }
         }
     }
